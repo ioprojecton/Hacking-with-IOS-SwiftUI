507c509c6e0f88908ec627abae423cc04aed101e
Surepic <46145992+ioprojecton@users.noreply.github.com>
2021-01-09 00:47:15 -0500

    MoonShot Challenge 7
    Xcassets folder is missing due to GitHub refusing to upload nested folders > 100

---
+++ Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Vahagn Martirosyan on 2021-01-07.
+//
+
+import Foundation
+
+struct Astronaut:Identifiable,Codable{
+    var id: String
+    var name: String
+    var description: String
+}
